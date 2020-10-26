c1d332559246edfade3ed8d8efaa0d9b06422c35
Ushinji <siguma971@gmail.com>
2020-10-26 17:32:00 +0900

    Create Button, List.

---
+++ src/components/molecules/SearchableSelect/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button class="button">Button</button>
+    <div class="panel is-primary">
+      <div class="panel-block">
+        <p class="control has-icons-left">
+          <input class="input is-primary" type="text" placeholder="Search" />
+          <span class="icon is-left">
+            <i class="material-icons">search</i>
+          </span>
+        </p>
+      </div>
+      <a class="panel-block is-active">bulma </a>
+      <a class="panel-block">marksheet </a>
+      <a class="panel-block">minireset.css </a>
+      <a class="panel-block">jgthms.github.io </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
