6e437b14cbbac3173dd88a7f26897d49ff465ea5
Ushinji <siguma971@gmail.com>
2020-10-27 17:49:38 +0900

    Fix Title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Hello World: {{ selected }}</h1>
+      <h1 class="title">Searchable Select</h1>
       <SearchableSelect
         :options="options"
         :value="selected"
